ef7e501aa62c8f669ef031bf592e1d349d00a80f
Diego Montivero <diego.montivero91@gmail.com>
2022-12-19 16:07:50 -0300

    Component now uses query params to fetch pages

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,26 +1,33 @@
-import type { Dispatch, FunctionComponent, MouseEvent, SetStateAction } from "react"
+import type { FC, MouseEvent } from "react"
+import { useRouter } from "next/router"
 import Style from "./Pagination.module.css"
 
 interface IProps {
-  page: number
   total: number
-  setPage: Dispatch<SetStateAction<number>>
 }
 
-export const Pagination: FunctionComponent<IProps> = ({ page, setPage, total }) => {
+export const Pagination: FC<IProps> = ({ total }) => {
+  const router = useRouter()
+  const page = Number(router.query?.page) || 1
   const totalPages = Math.ceil(total / 10)
 
   const handlePage = (event: MouseEvent<HTMLButtonElement>): void => {
-    setPage(Number(event.currentTarget.value))
+    router.replace({
+      pathname: router.pathname,
+      query: {
+        ...router.query,
+        page: event.currentTarget.value,
+      }
+    })
   }
 
   const renderButtons = () => {
     let buttons = []
 
-    for (let i = 0; i < totalPages; i++) {
+    for (let i = 1; i <= totalPages; i++) {
       buttons.push(
         <button key={i} onClick={handlePage} value={i} disabled={page === i}>
-          {i + 1}
+          {i}
         </button>
       )
     }
@@ -28,5 +35,9 @@ export const Pagination: FunctionComponent<IProps> = ({ page, setPage, total }) 
     return buttons
   }
 
-  return <div className={Style.pagination}>{renderButtons()}</div>
+  return (
+    <div className={Style.pagination}>
+      {renderButtons()}
+    </div>
+  )
 }
