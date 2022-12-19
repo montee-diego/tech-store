5e7fa76180712fb633ad6dd8e631b072c967cedc
Diego Montivero <diego.montivero91@gmail.com>
2022-12-19 16:06:11 -0300

    Updated ref elements and code cleanup

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,77 +1,36 @@
-import type { FunctionComponent, ChangeEvent, SyntheticEvent, SetStateAction } from "react"
-import { useRef, Dispatch } from "react"
+import type { FC, SyntheticEvent } from "react"
+import { useRef } from "react"
 import { useRouter } from "next/router"
 import Link from "next/link"
 
 import Style from "./Filter.module.css"
 
-interface IFilterOpts {
-  id: string
-  brands: string[]
-  minPrice: number
-  maxPrice: number
-  quantity: number
-  isSale: boolean
-}
-
 interface IProps {
   brands: string[]
-  id: string
-  setFilterOpts: Dispatch<SetStateAction<IFilterOpts>>
 }
 
-export const Filter: FunctionComponent<IProps> = ({ id, brands, setFilterOpts }) => {
+export const Filter: FC<IProps> = ({ brands }) => {
   const router = useRouter()
-  const minPrice = useRef<HTMLInputElement | null>(null)
-  const maxPrice = useRef<HTMLInputElement | null>(null)
-  const selectedBrands = useRef<string[]>([])
-  const stock = useRef<number>(0)
-  const isSale = useRef<boolean>(false)
+  const min = useRef<HTMLInputElement | null>(null)
+  const max = useRef<HTMLInputElement | null>(null)
+  const quantity = useRef<HTMLInputElement | null>(null)
+  const sale = useRef<HTMLInputElement | null>(null)
   const displayBrands = [...brands].sort()
-
-  function handleBrand(event: ChangeEvent<HTMLInputElement>): void {
-    const { value, checked } = event.target
-
-    if (checked) {
-      selectedBrands.current.push(value)
-    } else {
-      selectedBrands.current = selectedBrands.current.filter((item) => item !== value)
-    }
-  }
-
-  function handleStock(event: ChangeEvent<HTMLInputElement>): void {
-    stock.current = event.target.checked ? 1 : 0
-  }
-
-  function handleSale(event: ChangeEvent<HTMLInputElement>): void {
-    isSale.current = event.target.checked
-  }
 
   function handleSubmit(event: SyntheticEvent): void {
     event.preventDefault()
-
-    const url = new URL(window.location.href)
-
-    console.log(router.query.slug)
-
     router.replace({
-      pathname: url.pathname,
+      pathname: router.pathname,
       query: {
-        minPrice: Number(minPrice?.current?.value) || 0,
-        maxPrice: Number(maxPrice?.current?.value) || 500000,
-        quantity: stock.current,
-        isSale: isSale.current,
+        ...router.query,
+        min: Number(min?.current?.value) || 0,
+        max: Number(max?.current?.value) || 500000,
+        quantity: quantity?.current?.checked ? 1 : 0,
+        sale: sale?.current?.checked,
+        sort: router.query?.sort || 0,
+        page: 1,
       },
     })
-
-    // setFilterOpts({
-    //   id: id,
-    //   brands: selectedBrands.current.length ? selectedBrands.current : brands,
-    //   minPrice: Number(minPrice?.current?.value) || 0,
-    //   maxPrice: Number(maxPrice?.current?.value) || 500000,
-    //   quantity: stock.current,
-    //   isSale: isSale.current,
-    // })
   }
 
   return (
@@ -81,11 +40,9 @@ export const Filter: FunctionComponent<IProps> = ({ id, brands, setFilterOpts })
           <h5>Brands</h5>
           <div className={`${Style.checkbox} ${Style.filter}`}>
             {displayBrands.map((brand) => (
-              <Link href={`${router.query.slug}/${brand.toLowerCase()}`}>{brand}</Link>
-              // <label key={brand}>
-              //   <input type="checkbox" value={brand} onChange={handleBrand} />
-              //   {brand}
-              // </label>
+              <Link href={`/category/${router.query.slug}/${brand.toLowerCase()}`} key={brand}>
+                {brand}
+              </Link>
             ))}
           </div>
         </>
@@ -95,22 +52,22 @@ export const Filter: FunctionComponent<IProps> = ({ id, brands, setFilterOpts })
       <div className={Style.price}>
         <span className={Style.input}>
           <label htmlFor="min">Min</label>
-          <input type="number" id="min" min={0} max={500000} ref={minPrice} />
+          <input type="number" id="min" min={0} max={500000} ref={min} />
         </span>
         <span className={Style.input}>
           <label htmlFor="max">Max</label>
-          <input type="number" id="max" min={0} max={500000} ref={maxPrice} />
+          <input type="number" id="max" min={0} max={500000} ref={max} />
         </span>
       </div>
 
       <h5>Other</h5>
       <div className={`${Style.checkbox} ${Style.filter}`}>
         <label>
-          <input type="checkbox" name="stock" onChange={handleStock} />
+          <input type="checkbox" name="stock" ref={quantity} />
           In Stock
         </label>
         <label>
-          <input type="checkbox" name="sale" onChange={handleSale} />
+          <input type="checkbox" name="sale" ref={sale} />
           On Sale
         </label>
       </div>
