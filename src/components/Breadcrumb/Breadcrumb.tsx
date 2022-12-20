8b4f691d1ddd10df79a434a02db1d930ab3b52c0
Diego Montivero <diego.montivero91@gmail.com>
2022-12-20 16:53:39 -0300

    Refactor and modified logic and style

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -12,31 +12,33 @@ interface IProps {
     products: IProducts[]
     slug: string
   }
-  brand: string
+  brand?: string
 }
 
 export const Breadcrumb: FC<IProps> = ({ category, brand }) => {
   return (
-    <div className={`${Style.container} ${Style.breadcrumb}`}>
-      <div className={Style.breadcrumb}>
+    <div className={Style.Container}>
+      <div className={Style.Link}>
         <Link href="/">
           Home
         </Link>
-        <img className={Style.separator} src="/img/breadcrumb-sep.svg" alt="/" />
+        <img className={Style.Separator} src="/img/breadcrumb-sep.svg" alt="/" />
       </div>
 
-      <div className={Style.breadcrumb}>
+      <div className={Style.Link}>
         <Link href={`/category/${category.slug}`}>
           {category.name}
         </Link>
-        <img className={Style.separator} src="/img/breadcrumb-sep.svg" alt="/" />
       </div>
 
-      <div className={Style.breadcrumb}>
-        <Link href={`/category/${category.slug}/${brand.toLowerCase()}`}>
-          {brand}
-        </Link>
-      </div>
+      {brand && (
+        <div className={Style.Link}>
+          <img className={Style.Separator} src="/img/breadcrumb-sep.svg" alt="/" />
+          <Link href={`/category/${category.slug}/${brand.toLowerCase()}`}>
+            {brand}
+          </Link>
+        </div>
+      )}
     </div>
   )
 }
