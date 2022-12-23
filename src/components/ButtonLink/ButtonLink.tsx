61ba761031a1851f4413cfde2f4f630b12b58bd5
Diego Montivero <diego.montivero91@gmail.com>
2022-12-23 14:54:20 -0300

    Component now accepts and forwards ref

--- src/components/ButtonLink/ButtonLink.tsx
+++ src/components/ButtonLink/ButtonLink.tsx
@@ -1,4 +1,6 @@
-import type { FC, MouseEventHandler, ReactNode } from "react"
+import type { MouseEventHandler, ReactNode } from "react"
+
+import { forwardRef } from "react"
 
 import Link from "next/link"
 import Style from "./ButtonLink.module.css"
@@ -10,20 +12,25 @@ interface IProps {
   secondary?: boolean
 }
 
-export const ButtonLink: FC<IProps> = ({ children, href, onClick, secondary }) => {
+type Ref = HTMLAnchorElement & HTMLButtonElement
+
+export const ButtonLink = forwardRef<Ref, IProps>(function ButtonLink(props, ref) {
+  const { children, href, onClick, secondary } = props
   const buttonStyle = secondary ? Style.Secondary : Style.Primary
 
   return (
     <>
       {href ? (
         <Link href={href} passHref>
-          <a className={`${Style.Button} ${buttonStyle}`}>{children}</a>
+          <a className={`${Style.Button} ${buttonStyle}`} ref={ref}>
+            {children}
+          </a>
         </Link>
       ) : (
-        <button className={`${Style.Button} ${buttonStyle}`} onClick={onClick}>
+        <button className={`${Style.Button} ${buttonStyle}`} onClick={onClick} ref={ref}>
           {children}
         </button>
       )}
     </>
   )
-}
+})
